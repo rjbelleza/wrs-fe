264dce6805f2256d276dc85551faa4075e5a797b
rjbelleza <rjbelleza19@gmail.com>
2025-09-09 00:16:11 +0800

    Feature: Initialized login form

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,41 @@
+
+const LoginForm = () => {
+    
+    return (
+        <form className="flex flex-col border border-gray-400 shadow-md rounded-md">
+            <div className="flex justify-between items-center border-b border-gray-400 p-5">
+                <h1 className="text-xl font-AlbertSans-Bold">Login</h1>
+                <h1>logo</h1>
+            </div>
+            <div className="w-90 font-AlbertSans p-5 space-y-5">
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="email"
+                        id="username"
+                        name="username"
+                        className="border border-gray-400 rounded py-2 px-5"
+                        placeholder="Enter username here.."
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="border border-gray-400 rounded py-2 px-5"
+                        placeholder="Enter password here.."
+                    />
+                </div>
+                <button
+                    className="w-full bg-black text-white rounded font-AlbertSans py-2 cursor-pointer"
+                >
+                    Login
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default LoginForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
+import LoginForm from "../components/auth/LoginForm";
 
 const LoginPage = () => {
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <h1 className="text-2xl mb-3 font-AlbertSans-Bold">Aqua Springs</h1>
-            <h4>POS System</h4>
+            <h4 className="font-AlbertSans mb-10">POS System</h4>
+            <LoginForm />
         </div>
     )
 }
