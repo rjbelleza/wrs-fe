ba5649e4697672bee18b6b9832ac4b7af151793c
rjbelleza <rjbelleza19@gmail.com>
2025-09-09 21:37:14 +0800

    Feat: Login page UI

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -2,10 +2,10 @@
 const LoginForm = () => {
     
     return (
-        <form className="flex flex-col border border-gray-400 shadow-md rounded-md">
+        <form className="flex flex-col bg-white border border-gray-400 shadow-md rounded-md">
             <div className="flex justify-between items-center border-b border-gray-400 p-5">
-                <h1 className="text-xl font-AlbertSans-Bold">Login</h1>
-                <h1>logo</h1>
+                <h1 className="text-xl text-primary font-AlbertSans-Bold">Login</h1>
+                <img src="src/assets/images/logo.png" alt="logo" width={50} />
             </div>
             <div className="w-90 font-AlbertSans p-5 space-y-5">
                 <div className="flex flex-col gap-2">
@@ -29,7 +29,7 @@ const LoginForm = () => {
                     />
                 </div>
                 <button
-                    className="w-full bg-black text-white rounded font-AlbertSans py-2 cursor-pointer"
+                    className="w-full bg-primary hover:bg-primary-hover text-white rounded font-AlbertSans py-2 cursor-pointer"
                 >
                     Login
                 </button>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,9 +3,11 @@ import LoginForm from "../components/auth/LoginForm";
 const LoginPage = () => {
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <h1 className="text-2xl mb-3 font-AlbertSans-Bold">Aqua Springs</h1>
-            <h4 className="font-AlbertSans mb-10">POS System</h4>
+        <div className="flex flex-col md:justify-center items-center h-screen pt-20 md:pt-0 
+                        bg-[url('src/assets/images/bg2.png')] md:bg-[url('src/assets/images/bg1.png')] bg-no-repeat md:bg-center md:bg-cover"
+        >
+            <h1 className="text-2xl text-primary mb-3 font-AlbertSans-Bold">Aqua Springs</h1>
+            <h4 className="text-gray-600 font-AlbertSans mb-10">POS System</h4>
             <LoginForm />
         </div>
     )
