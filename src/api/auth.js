e2d9fadf73dee6c7ac38ea47ad6e9a830aad43bb
rjbelleza <rjbelleza19@gmail.com>
2025-09-27 17:52:09 +0800

    Feat: Login and Logout API call

---
+++ src/api/auth.js
@@ -0,0 +1,36 @@
+import backendApi from "@/api/backendApi";
+import axios from "axios";
+
+export const loginUser = async ({ credentials }) => {
+    try {
+        const res = await backendApi.post('/login', credentials);
+
+        return res.data;
+    } catch (err) {
+        let errorMessage = "Login failed. Please try again.";
+
+        if (axios.isAxiosError(err) && err.response) {
+            errorMessage = err.response.data.error || errorMessage;
+        }
+
+        console.error("Error during login: ", err);
+        return errorMessage;
+    }
+};
+
+export const logoutUser = async () => {
+    try {
+        const res = await backendApi.post('/logout');
+
+        return res.data;
+    } catch (err) {
+        let errorMessage = "Logout failed. Please try again.";
+
+        if (axios.isAxiosError(err) && err.response) {
+            errorMessage = err.response.data.error || errorMessage;
+        }
+
+        console.error("Error during logout: ", err);
+        return errorMessage;
+    }
+};
