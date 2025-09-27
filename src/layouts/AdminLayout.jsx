2f1172b53b87c88359885b769a91c8401f2e2eac
rjbelleza <rjbelleza19@gmail.com>
2025-09-27 17:06:17 +0800

    Update: jsconfig.json

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom';
 
 const AdminLayout = () => {
 
     return (
         <div>
             Admin Layout
+            <Outlet />
         </div>
     );
 };
